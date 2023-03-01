b8e0c7d71791338e7d104079ccecf3d8073a58c4
msintas <59841444+WilderSea@users.noreply.github.com>
2023-03-01 17:34:03 +0100

    Update inter.c

--- inter.c
+++ inter.c
@@ -1,8 +1,4 @@
-// still to fix
-
-
 #include <unistd.h>
-#include <stdio.h>
 
 
 int ft_strchr(char *string, char c)
@@ -23,20 +19,17 @@ int ft_strchr(char *string, char c)
 
 int ft_revstrchr(char *string, char c, int pos)
 {
+    pos = pos - 1;
     while (pos >= 0)
     {
-        printf("pos = %d\n", pos);
-        printf("string[pos] = %c\n", string[pos]);
         if (string[pos] == c)
         {
-            printf("encontre\n");
             return (1);
         }
         pos--;
     }
     return (0);
 }
-
 
 void ft_inter(char *str_one, char *str_two)
 {
@@ -46,24 +39,17 @@ void ft_inter(char *str_one, char *str_two)
     i = 0;
     while(str_one[i])
     {
-        // check if char is in str_two
         if(ft_strchr(str_two, str_one[i]) == 1)
         {
-            // check before writing if char is not already in string one
             if (ft_revstrchr(str_one, str_one[i], i) == 0)
             {
-                printf("i = %d\n", i);
                 write(1, &str_one[i], 1);
-                write(1, "\n", 1);
             }
-            
         }
-        
         i++;
     }
     write(1, "\n", 1);
 }
-
 
 int main(int argc, char **argv)
 {
@@ -75,6 +61,5 @@ int main(int argc, char **argv)
     {
         write(1, "\n", 1);
     }
-    
     return (0);
 }
