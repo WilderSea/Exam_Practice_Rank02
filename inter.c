10c4a9151b6301168ccbb9d147fb949b22c29bfc
msintas <59841444+WilderSea@users.noreply.github.com>
2023-03-01 17:48:43 +0100

    Update inter.c

--- inter.c
+++ inter.c
@@ -19,6 +19,7 @@ int ft_strchr(char *string, char c)
 
 int ft_revstrchr(char *string, char c, int pos)
 {
+    // from position to beggining to check if char already exists
     pos = pos - 1;
     while (pos >= 0)
     {
@@ -39,8 +40,10 @@ void ft_inter(char *str_one, char *str_two)
     i = 0;
     while(str_one[i])
     {
+        // check if char is in str_two
         if(ft_strchr(str_two, str_one[i]) == 1)
         {
+            // check before writing if char is not already in string one
             if (ft_revstrchr(str_one, str_one[i], i) == 0)
             {
                 write(1, &str_one[i], 1);
